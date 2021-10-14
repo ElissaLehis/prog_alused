477ca3970df422f38c803505d0d680ad3c6e7cfe
Elissa Lehis <elissa.lehis@khk.ee>
2021-10-14 09:45:15 +0300

    Teksti väljastamine konsooli ja kommentaarimine

--- app.js
+++ app.js
@@ -7,3 +7,17 @@ console.log('See ' + 'on ' + 'liitlause ')
 /* mitme realine kommentaar
 
  */
+console.log('2016. aastal valiti Eesti presidendiks Kersti Kaljulaid')
+
+//muutujad ja andmetüübid
+// 1.deklareerimine
+// let muutujanimimi
+// 2. väärtuse omistamine
+// muutujanimi = väärtus
+
+let aasta
+aasta = 2016
+let president
+president = 'Kersti Kaljulaid'
+
+console.log(aasta + '. Eesti presidendiks valiti ' + president)
